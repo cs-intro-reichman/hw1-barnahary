bde51c5ccf2f39a45616f09fd115d1a60eeeb60f
barnahary <bar.nahary@post.runi.ac.il>
2024-11-10 15:44:13 +0200

    submission

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,20 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		double a = Double.parseDouble(args[0]) * Math.random();
+		double b = Double.parseDouble(args[0]) * Math.random();
+		double c = Double.parseDouble(args[0]) * Math.random();
+		double maxNumber = Math.max (a, Math.max(b, c));
+		double minNumber = Math.min (a, Math.min(b, c));
+		double midNumber = (a + b + c - maxNumber - minNumber);
+		int roundedA =(int) Math.round( a );
+		int roundedB = (int) Math.round( b );
+		int roundedC = (int) Math.round( c );
+		int roundedMax = (int) Math.round( maxNumber );
+		int roundedMid = (int) Math.round( midNumber );
+		int roundedMin = (int) Math.round( minNumber );
+		System.out.println(roundedA + " " + roundedB + " " + roundedC);
+		System.out.println(roundedMax + " " + roundedMid  + " " + roundedMin );
+
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,13 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		int deposit = Integer.parseInt(args[0]);
+		double ratePresentage = Double.parseDouble(args[1]);
+		int years = Integer.parseInt(args[2]);
+		double outcome = 1 + (ratePresentage / 100.0);
+		double result = Math.pow(outcome, years);
+		double total = deposit * result;
+		int roundedTotal = (int) Math.round(total);
+		System.out.println(" After " + years + " years, " + deposit + "$ saved at " + ratePresentage + "% will yield " + roundedTotal + "$");
 	}
 }
